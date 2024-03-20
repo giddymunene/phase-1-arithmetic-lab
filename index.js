23a4d66738bba28463ee0d976d79526514f02cd2
gideon <gideonmunene0@gmail.com>
2024-03-20 08:57:56 -0400

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply 
+    let num1 = 2 ;
+    let num2 = 31 ;
+    multiply = num1*num2;
 
-// Write your code here
+
+let random
+random = Math.floor(Math.random() * 10) + 1;
+let mod
+    let num3 = 8 ;
+    let num4 = 2 ;
+    mod = num3/num4;
+
+let max
+max = Math.max(1,9,12,17,20)
